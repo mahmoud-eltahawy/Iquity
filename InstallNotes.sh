e86c0370fff0133a71130b4bf9d4e6386213f247
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-07-14 00:12:58 +0300

    cleanups

--- InstallNotes.sh
+++ InstallNotes.sh
@@ -1,12 +1,11 @@
-
 cd $srcdir/${pkgname}-${pkgver}
-rustup update stable
-rustup component add rust-src --toolchain stable
+rustup update nightly-2023-07-07-x86_64-unknown-linux-gnu
+rustup component add rust-src --toolchain nightly-2023-07-07-x86_64-unknown-linux-gnu
 cargo install trunk
 cargo install tauri-cli
 rustup target add wasm32-unknown-unknown
 cd frontend
-bun install
-bunx tailwindcss -i ./css/input.css -o ./css/output.css --minify
+npm install
+npx tailwindcss -i ./css/input.css -o ./css/output.css --minify
 cd ..
 cargo tauri build -b none --target x86_64-unknown-linux-gnu -- -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
