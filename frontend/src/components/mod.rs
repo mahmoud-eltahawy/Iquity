c5143fcd825f4ea3776638d6f2f8cae0c59459f5
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-07-17 01:05:06 +0300

    refactor

--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -1,5 +1,4 @@
 // pub mod btn;
 pub mod container;
-pub mod dual_view;
 pub mod editor;
 pub mod markdown_preview;
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,18 +2,41 @@ pub mod components;
 pub mod contexts;
 pub mod tauri;
 
-use components::dual_view::{editor_view, preview_view};
+use components::container::style_container;
 use leptos::prelude::*;
 use leptos_router::components::{Route, Router, Routes};
 use leptos_router::StaticSegment;
 
+use crate::components::container::container;
+use crate::components::editor::editor;
+use crate::components::markdown_preview::markdown_preview;
 use crate::contexts::config::config_provider;
 use crate::contexts::markdown::markdown_provider;
+use leptos::html::div;
+
+pub fn editor_view() -> impl IntoView {
+    provide_context(markdown_provider());
+    style_container(
+    div()
+        .class("w-[calc(100vw-2.5rem)] flex flex-1 flex-row justify-center space-x-8 items-center h-[calc(100vh-8.5rem)]")
+        .child(
+            container(editor()),
+        ))
+}
+
+pub fn preview_view() -> impl IntoView {
+    provide_context(markdown_provider()); //TODO :it should recieve it not creating it
+    style_container(
+    div()
+        .class("w-[calc(100vw-2.5rem)] flex flex-1 flex-row justify-center space-x-8 items-center h-[calc(100vh-8.5rem)]")
+        .child(
+            container(markdown_preview())
+        ))
+}
 
 fn app() -> impl IntoView {
     let conf = config_provider();
     provide_context(conf);
-    provide_context(markdown_provider());
 
     let theme = move || conf.get().theme;
     view! {
