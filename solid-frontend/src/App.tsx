2f28183c3252f131d8bbd52b1f371e13a962fc92
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-07-17 02:18:01 +0300

    try solidjs

---
+++ solid-frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { createSignal } from 'solid-js'
+import solidLogo from './assets/solid.svg'
+import viteLogo from '/vite.svg'
+
+export function Editor() {
+
+  return (
+    <>
+      <h1 class="bg-black text-white">Editor</h1>
+    </>
+  )
+}
+
+export function Preview() {
+
+  return (
+    <>
+      <h1 class="bg-black text-white">Preview</h1>
+    </>
+  )
+}
---
+++ solid-frontend/tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+    plugins: [
+        require('daisyui'),
+        require('@tailwindcss/typography')
+    ],
+    theme: {
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+            display: ['Comfortaa', 'display'],
+            mono: ['Fira Mono', 'mono']
+        },
+    },
+    daisyui: {
+        themes: ["light", "dark", "cupcake",
+            "bumblebee", "emerald", "corporate",
+            "synthwave", "retro", "cyberpunk",
+            "valentine", "halloween", "garden",
+            "forest", "aqua", "lofi",
+            "pastel", "fantasy", "wireframe",
+            "black", "luxury", "dracula",
+            "cmyk", "autumn", "business",
+            "acid", "lemonade", "night",
+            "coffee", "winter", {
+                ubiquity: {
+                    "primary": "#38BDF8",
+                    "secondary": "#818CF8",
+                    "accent": "#F471B5",
+                    "neutral": "#1E293B",
+                    "base-100": "#0F172A",
+                    "info": "#0CA5E9",
+                    "success": "#2DD4BF",
+                    "warning": "#F4BF50",
+                    "error": "#FB7085",
+                },
+            }
+        ],
+    }
+}
+
