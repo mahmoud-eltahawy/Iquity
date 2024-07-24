c68accc057ecd48aa707096f2b48e6ff06b8a531
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-07-24 03:09:49 +0300

    remove mdtg crate

--- ubi-crates/config/src/error.rs
+++ ubi-crates/config/src/error.rs
@@ -5,12 +5,4 @@ use thiserror::Error;
 pub enum ConfigError {
     #[error(transparent)]
     Io(#[from] io::Error),
-
-    // #[error("Error deserialising RON to string.")]
-    // RonToStringError(#[from] ron::de::Error),
-    #[error("Error deserialising RON to string.")]
-    RonToString(#[from] ron::de::SpannedError),
-
-    #[error("An error occured with RON.")]
-    Ron(#[from] ron::Error),
 }
