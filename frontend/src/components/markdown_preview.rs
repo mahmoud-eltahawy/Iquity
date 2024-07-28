f981c06ce6865baaa99cb381ee478a896b06b514
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-07-28 16:59:51 +0300

    themes and clean unused code

--- frontend/src/components/markdown_preview.rs
+++ frontend/src/components/markdown_preview.rs
@@ -11,15 +11,9 @@ pub fn markdown_preview() -> impl IntoView {
     let md = move || markdown.content.get();
 
     div()
-        .class(
-            "flex flex-col h-full overflow-visible scroll-smooth h-screen w-screen p-5",
-        )
-        .child(
-            div()
-                .attr("class", "overflow-auto")
-                .child(article()
-                    .id("preview")
-                    .class("prose-img:rounded-xl prose-pre:bg-base-300 prose-pre:text-base-content prose-pre:overflow-auto prose-code:bg-base-300 prose-code:px-[5.5px] prose-code:font-normal prose-code:rounded-[0.3125rem] prose-code:overflow-auto prose-a:no-underline prose-a:text-info print:block")
-                    .inner_html(md)),
-        )
+        .attr("class", "overflow-auto")
+        .child(article()
+            .id("preview")
+            .class("absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 prose-img:rounded-xl prose-pre:bg-base-300 prose-pre:text-base-content prose-pre:overflow-auto prose-code:bg-base-300 prose-code:px-[5.5px] prose-code:font-normal prose-code:rounded-[0.3125rem] prose-code:overflow-auto prose-a:no-underline prose-a:text-info print:block")
+            .inner_html(md))
 }
