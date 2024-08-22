43f404f0ac12136847dfe30867732c8637f87ce8
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-08-22 22:36:31 +0300

    ugly solution

--- crates/styling/src/color.rs
+++ crates/styling/src/color.rs
@@ -10,11 +10,10 @@ use super::Style;
 
 use paste::paste;
 
-macro_rules! color_impl {
-    ($target:ident,$output:ident,{$($color:ident),+}) => {
+macro_rules! color_define {
+    ($($color:ident),+) => {
         paste! {
         #[derive(Hash, Eq, PartialEq)]
-        #[allow(non_camel_case_types)]
         pub enum Color {
             Hex(u32),
             THex(u32),
@@ -24,6 +23,45 @@ macro_rules! color_impl {
             Hsla(u16, u8, u8, u8)
             $(,$color)*
         }
+
+        impl Display for Color {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let result = match self {
+                    Color::Hex(c) => {
+                        let result = format!("{c:#06x}")[2..].to_string();
+                        format!("#{result}")
+                    }
+                    Color::THex(c) => {
+                        let result = format!("{c:#08x}")[2..].to_string();
+                        format!("#{result}")
+                    }
+                    Color::Rgb(red, green, blue) => format!("rgb({red},{green},{blue})"),
+                    Color::Rgba(red, green, blue, opacity) => {
+                        format!("rgba({red},{green},{blue},{})", *opacity as f32 / 100.)
+                    }
+                    Color::Hsl(hue, saturation, lightness) => {
+                        format!("hsl({hue},{saturation}%,{lightness}%)")
+                    }
+                    Color::Hsla(hue, saturation, lightness, opacity) => {
+                        format!(
+                            "hsl({hue},{saturation}%,{lightness}%,{})",
+                            *opacity as f32 / 100.
+                        )
+                    }
+                    $(
+                        Color::$color =>  stringify!([<$color:lower>]).to_string(),
+                    )*
+                    };
+                write!(f, "{}", result)
+            }
+        }
+        }
+    };
+}
+
+macro_rules! color_impl {
+    ($target:ident,$output:ident,{$($color:ident),+}) => {
+        paste! {
         impl Style<$target<Color>> {
             pub fn hex(self, hex: u32) -> Style<$output> {
                 self.base(Color::Hex(hex))
@@ -69,47 +107,305 @@ macro_rules! color_impl {
                 }
             )*
         }
-
-        impl Display for Color {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                let result = match self {
-                    Color::Hex(c) => {
-                        let result = format!("{c:#06x}")[2..].to_string();
-                        format!("#{result}")
-                    }
-                    Color::THex(c) => {
-                        let result = format!("{c:#08x}")[2..].to_string();
-                        format!("#{result}")
-                    }
-                    Color::Rgb(red, green, blue) => format!("rgb({red},{green},{blue})"),
-                    Color::Rgba(red, green, blue, opacity) => {
-                        format!("rgba({red},{green},{blue},{})", *opacity as f32 / 100.)
-                    }
-                    Color::Hsl(hue, saturation, lightness) => {
-                        format!("hsl({hue},{saturation}%,{lightness}%)")
-                    }
-                    Color::Hsla(hue, saturation, lightness, opacity) => {
-                        format!(
-                            "hsl({hue},{saturation}%,{lightness}%,{})",
-                            *opacity as f32 / 100.
-                        )
-                    }
-                    $(
-                        Color::$color =>  stringify!([<$color:lower>]).to_string(),
-                    )*
-                    };
-                write!(f, "{}", result)
-            }
-        }
         }
     };
 }
 
 // PreStyleBase,
 // StyleBaseState,
+color_define!(
+    ALICEBLUE,
+    ANTIQUEWHITE,
+    AQUA,
+    AQUAMARINE,
+    AZURE,
+    BEIGE,
+    BISQUE,
+    BLACK,
+    BLANCHEDALMOND,
+    BLUE,
+    BLUEVIOLET,
+    BROWN,
+    BURLYWOOD,
+    CADETBLUE,
+    CHARTREUSE,
+    CHOCOLATE,
+    CORAL,
+    CORNFLOWERBLUE,
+    CORNSILK,
+    CRIMSON,
+    CYAN,
+    DARKBLUE,
+    DARKCYAN,
+    DARKGOLDENROD,
+    DARKGREY,
+    DARKGREEN,
+    DARKKHAKI,
+    DARKMAGENTA,
+    DARKOLIVEGREEN,
+    DARKORANGE,
+    DARKORCHID,
+    DARKRED,
+    DARKSALMON,
+    DARKSEAGREEN,
+    DARKSLATEBLUE,
+    DARKSLATEGREY,
+    DARKTURQUOISE,
+    DARKVIOLET,
+    DEEPPINK,
+    DEEPSKYBLUE,
+    DIMGRAY,
+    DODGERBLUE,
+    FIREBRICK,
+    FLORALWHITE,
+    FORESTGREEN,
+    FUCHSIA,
+    GAINSBORO,
+    GHOSTWHITE,
+    GOLD,
+    GOLDENROD,
+    GREY,
+    GREEN,
+    GREENYELLOW,
+    HONEYDEW,
+    HOTPINK,
+    INDIANRED,
+    INDIGO,
+    IVORY,
+    KHAKI,
+    LAVENDER,
+    LAVENDERBLUSH,
+    LAWNGREEN,
+    LEMONCHIFFON,
+    LIGHTBLUE,
+    LIGHTCORAL,
+    LIGHTCYAN,
+    LIGHTGOLDENRODYELLOW,
+    LIGHTGREY,
+    LIGHTGREEN,
+    LIGHTPINK,
+    LIGHTSALMON,
+    LIGHTSEAGREEN,
+    LIGHTSKYBLUE,
+    LIGHTSLATEGREY,
+    LIGHTSTEELBLUE,
+    LIGHTYELLOW,
+    LIME,
+    LIMEGREEN,
+    LINEN,
+    MAGENTA,
+    MAROON,
+    MEDIUMAQUAMARINE,
+    MEDIUMBLUE,
+    MEDIUMORCHID,
+    MEDIUMPURPLE,
+    MEDIUMSEAGREEN,
+    MEDIUMSLATEBLUE,
+    MEDIUMSPRINGGREEN,
+    MEDIUMTURQUOISE,
+    MEDIUMVIOLETRED,
+    MIDNIGHTBLUE,
+    MINTCREAM,
+    MISTYROSE,
+    MOCCASIN,
+    NAVAJOWHITE,
+    NAVY,
+    OLDLACE,
+    OLIVE,
+    OLIVEDRAB,
+    ORANGE,
+    ORANGERED,
+    ORCHID,
+    PALEGOLDENROD,
+    PALEGREEN,
+    PALETURQUOISE,
+    PALEVIOLETRED,
+    PAPAYAWHIP,
+    PEACHPUFF,
+    PERU,
+    PINK,
+    PLUM,
+    POWDERBLUE,
+    PURPLE,
+    RED,
+    ROSYBROWN,
+    ROYALBLUE,
+    SADDLEBROWN,
+    SALMON,
+    SANDYBROWN,
+    SEAGREEN,
+    SEASHELL,
+    SIENNA,
+    SILVER,
+    SKYBLUE,
+    SLATEBLUE,
+    SLATEGREY,
+    SNOW,
+    SPRINGGREEN,
+    STEELBLUE,
+    TAN,
+    TEAL,
+    THISTLE,
+    TOMATO,
+    TURQUOISE,
+    VIOLET,
+    WHEAT,
+    WHITE,
+    WHITESMOKE,
+    YELLOW,
+    YELLOWGREEN
+);
+
 color_impl!(
     PreBackgroundBase,
     BackgroundBaseState,
+    {
+        ALICEBLUE,
+        ANTIQUEWHITE,
+        AQUA,
+        AQUAMARINE,
+        AZURE,
+        BEIGE,
+        BISQUE,
+        BLACK,
+        BLANCHEDALMOND,
+        BLUE,
+        BLUEVIOLET,
+        BROWN,
+        BURLYWOOD,
+        CADETBLUE,
+        CHARTREUSE,
+        CHOCOLATE,
+        CORAL,
+        CORNFLOWERBLUE,
+        CORNSILK,
+        CRIMSON,
+        CYAN,
+        DARKBLUE,
+        DARKCYAN,
+        DARKGOLDENROD,
+        DARKGREY,
+        DARKGREEN,
+        DARKKHAKI,
+        DARKMAGENTA,
+        DARKOLIVEGREEN,
+        DARKORANGE,
+        DARKORCHID,
+        DARKRED,
+        DARKSALMON,
+        DARKSEAGREEN,
+        DARKSLATEBLUE,
+        DARKSLATEGREY,
+        DARKTURQUOISE,
+        DARKVIOLET,
+        DEEPPINK,
+        DEEPSKYBLUE,
+        DIMGRAY,
+        DODGERBLUE,
+        FIREBRICK,
+        FLORALWHITE,
+        FORESTGREEN,
+        FUCHSIA,
+        GAINSBORO,
+        GHOSTWHITE,
+        GOLD,
+        GOLDENROD,
+        GREY,
+        GREEN,
+        GREENYELLOW,
+        HONEYDEW,
+        HOTPINK,
+        INDIANRED,
+        INDIGO,
+        IVORY,
+        KHAKI,
+        LAVENDER,
+        LAVENDERBLUSH,
+        LAWNGREEN,
+        LEMONCHIFFON,
+        LIGHTBLUE,
+        LIGHTCORAL,
+        LIGHTCYAN,
+        LIGHTGOLDENRODYELLOW,
+        LIGHTGREY,
+        LIGHTGREEN,
+        LIGHTPINK,
+        LIGHTSALMON,
+        LIGHTSEAGREEN,
+        LIGHTSKYBLUE,
+        LIGHTSLATEGREY,
+        LIGHTSTEELBLUE,
+        LIGHTYELLOW,
+        LIME,
+        LIMEGREEN,
+        LINEN,
+        MAGENTA,
+        MAROON,
+        MEDIUMAQUAMARINE,
+        MEDIUMBLUE,
+        MEDIUMORCHID,
+        MEDIUMPURPLE,
+        MEDIUMSEAGREEN,
+        MEDIUMSLATEBLUE,
+        MEDIUMSPRINGGREEN,
+        MEDIUMTURQUOISE,
+        MEDIUMVIOLETRED,
+        MIDNIGHTBLUE,
+        MINTCREAM,
+        MISTYROSE,
+        MOCCASIN,
+        NAVAJOWHITE,
+        NAVY,
+        OLDLACE,
+        OLIVE,
+        OLIVEDRAB,
+        ORANGE,
+        ORANGERED,
+        ORCHID,
+        PALEGOLDENROD,
+        PALEGREEN,
+        PALETURQUOISE,
+        PALEVIOLETRED,
+        PAPAYAWHIP,
+        PEACHPUFF,
+        PERU,
+        PINK,
+        PLUM,
+        POWDERBLUE,
+        PURPLE,
+        RED,
+        ROSYBROWN,
+        ROYALBLUE,
+        SADDLEBROWN,
+        SALMON,
+        SANDYBROWN,
+        SEAGREEN,
+        SEASHELL,
+        SIENNA,
+        SILVER,
+        SKYBLUE,
+        SLATEBLUE,
+        SLATEGREY,
+        SNOW,
+        SPRINGGREEN,
+        STEELBLUE,
+        TAN,
+        TEAL,
+        THISTLE,
+        TOMATO,
+        TURQUOISE,
+        VIOLET,
+        WHEAT,
+        WHITE,
+        WHITESMOKE,
+        YELLOW,
+        YELLOWGREEN
+    }
+);
+
+color_impl!(
+    PreStyleBase,
+    StyleBaseState,
     {
         ALICEBLUE,
         ANTIQUEWHITE,
